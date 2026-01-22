4579dddf57920bc6892a04793b9a39f2eb7ad3d3
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2026-01-22 23:01:37 +0530

    Added OrganizerDashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import React from "react";
+import OrganizerDashboard from "./components/Organizer";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/organizer" element={<OrganizerDashboard />} />
       </Routes>
     </BrowserRouter>
   );
