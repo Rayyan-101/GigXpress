ab9acb4c07e6d9b01c8ecc96e9fd04351f5b08eb
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2026-01-26 10:24:01 +0530

    Updated file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/organizer" element={<OrganizerDashboard />} />
-        <Route path="/volunteer" element={<WorkerDashboard />} />
+        
 
       </Routes>
     </BrowserRouter>
