1437fcfdc6a884a838a4bebdcc77da90b38676bd
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2026-03-26 21:38:39 +0530

    Added Admin Dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import OrganizerDashboard from "./components/Organizer";
 import WorkerDashboard from "./components/Volunteer";
 import JoinPage from "./components/JoinPage";
 import CompleteRegistrationFlow from "./components/CompleteRegistrationFlow";
+import AdminDashboard from "./components/AdminDashboard";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/organizer" element={<OrganizerDashboard />} />
         <Route path="/volunteer" element={<WorkerDashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/signup" element={<CompleteRegistrationFlow />} />
       </Routes>
     </BrowserRouter>
