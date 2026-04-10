53420b0882c3c3b0a1e71d7756539f70ddd82216
Rayyan-101 <rayyanlulaniya1990@gmail.com>
2026-04-10 23:04:45 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,30 @@ const server = http.createServer(app);
 const io     = initSocket(server);
 app.set('io', io);
 
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "https://gig-xpress-wu8e.vercel.app"
+];
 // Middleware
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5000',
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed: " + origin));
+    }
+  },
   credentials: true
 }));
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || 'http://localhost:5000',
+//   credentials: true
+// }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
